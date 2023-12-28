45fd6d204a9d791fef46df253b1ef89fa64b9df1
Yuval Cohen <yuvalco1@gmail.com>
2023-12-28 16:17:15 +0200

    first commit

---
+++ src/Question.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+public class Question {
+
+    private String questionText;
+
+    private List<String> options;
+
+    private int correctOptionIndex;
+
+
+    public Question(String questionText, List<String> options, int correctOptionIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public int getCorrectOptionIndex() {
+        return correctOptionIndex;
+    }
+
+    public void setCorrectOptionIndex(int correctOptionIndex) {
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public void displayQuestion(){
+        System.out.println(this.questionText);
+        for (int i = 0; i < this.options.size(); i++) {
+            System.out.println(this.options.get(i));
+        }
+    }
+
+    public boolean isCorrectAnswer(int answerIndex){
+        return answerIndex == this.correctOptionIndex;
+    }
+
+}
