387f4587b5e742ca95bb056caa9990912a826953
Yuval Cohen <yuvalco1@gmail.com>
2024-01-08 20:37:22 +0200

    Add documentation to QuizTaker

--- src/QuizTaker.java
+++ src/QuizTaker.java
@@ -7,42 +7,69 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class represents a quiz taker.
+ */
 public class QuizTaker {
 
+    /**
+     * The name of the quiz taker.
+     */
     private String name;
 
+    /**
+     * Constructor to initialize the quiz taker's name.
+     *
+     * @param name The name of the quiz taker
+     */
     public QuizTaker(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
+    // Getters and setters for the name property
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    /**
+     * Administers the given quiz to this quiz taker.
+     *
+     * Displays welcome message, prompts for answers to each question,
+     * scores the quiz based on the answers, and prints the final result.
+     *
+     * @param quiz The quiz to be taken
+     * @return The score as a percentage
+     */
     public double takeQuiz(Quiz quiz) {
 
+        // Print welcome message
         System.out.println("Hello " + this.name + ". Welcome to Yuval's Quiz !!!");
-        System.out.println("You will be presented with 10 random question, please answer each question with numbers only 1-4");
+        System.out.println("You will be presented with 10 random questions, please answer each question with numbers only 1-4");
 
+        // Store quiz answers
         ArrayList<Integer> playerAnswers = new ArrayList<>();
+
+        // Scanner to get user input
         Scanner scanner = new Scanner(System.in);
 
+        // Loop through questions
         for (Question question : quiz.questions) {
+
+            // Display question
             question.displayQuestion();
+
+            // Get answer input
             int answer = 0;
             while (answer < 1 || answer > 4) {
                 System.out.println("Please enter a valid answer (1-4): ");
                 answer = scanner.nextInt();
             }
+
+            // Add answer to list
             playerAnswers.add(answer);
         }
-        //scanner.close();
+
+        // Score quiz and print result
         System.out.println("Your score is: " + quiz.scoreQuiz(playerAnswers) + "%");
         return quiz.scoreQuiz(playerAnswers);
+
     }
 
 
